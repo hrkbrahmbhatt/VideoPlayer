d1df68e6458071f97dcec6d700f1114604847ed6
hrk <hrkbrahmbhatt@gmail.com>
2018-03-20 19:36:48 -0400

    Initial commit

---
+++ app/src/main/java/my/webs2canada/videoplayer/MainActivity.java
@@ -0,0 +1,77 @@
+package my.webs2canada.videoplayer;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    private VideoView myVideoView;
+    private int position = 0;
+    private ProgressDialog progressDialog;
+    private MediaController mediaControls;
+
+   String Videos_URL = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        myVideoView = (VideoView)findViewById(R.id.videoView);
+        MediaController vidControl = new MediaController(this);
+
+        if (mediaControls == null) {
+            mediaControls = new MediaController(this);
+        }
+
+        // Find your VideoView in your video_main.xml layout
+        myVideoView = (VideoView) findViewById(R.id.videoView);
+
+        // Create a progressbar
+        progressDialog = new ProgressDialog(this);
+        // Set progressbar title
+        progressDialog.setTitle("Streaming");
+        // Set progressbar message
+        progressDialog.setMessage("Loading...");
+
+        progressDialog.setCancelable(false);
+        // Show progressbar
+        progressDialog.show();
+
+        try {
+            Uri video = Uri.parse(Videos_URL);
+            myVideoView.setVideoURI(video);
+            myVideoView.setMediaController(mediaControls);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        myVideoView.requestFocus();
+        myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                progressDialog.dismiss();
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    myVideoView.pause();
+                }
+            }
+        });
+
+    }
+}
